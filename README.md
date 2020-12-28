04ace4f73de7f850b4b7e56dbabfcc87fe016cc5
GramingFoxTeam <ibrahimbozkurt0270@gmail.com>
2020-12-28 16:57:50 +0300

    TBL

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tbl-api.js
  Turkey Bot List API Library.
+# How to use
+Example: ```js
+const TBL = require("tbl-api.js");
+const tbl = new("TBLTOKEN", client);
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+const snekfetch = require('snekfetch');
+const API = 'https://turkeybotlist.herokuapp.com/api/';
+
+class TBLAPI {
+  constructor(token, client) {
+    this.token = token;
+    if (client) {
+      this.client = client;
+      client.on('ready', () => {
+        this.postStats();
+        setInterval(() => {
+          this.postStats();
+        }, 1800000);
+      });
+    }
+  }
+
+  _request(method, endpoint, data, auth) {
+    const request = snekfetch[method](API + endpoint);
+    if (method === 'post' && data) request.send(data);
+    if (method === 'get' && data) request.query(data);
+    if (auth) request.set({ Authorization: this.token });
+    return request;
+  }
+
+  async postStats(serverCount, shardId, shardCount) {
+    if (!this.token) throw new Error('This function requires a token to be set');
+    if (!serverCount && !this.client) throw new Error('postStats requires 1 argument');
+    const data = {};
+    if (serverCount) {
+      data.server_count = serverCount;
+    } else {
+      data.server_count = this.client.guilds.cache.size;
+     
+    }
+    const response = await this._request('post', `auth/stats/${this.client.user.id}`, data, true);
+    console.log("Server Count Posted to TBL!")
+    return response.body;
+  }
+
+}
+
+module.exports = TBLAPI;
